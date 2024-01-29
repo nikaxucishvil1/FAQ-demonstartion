c5d7c5a0faa9eaf6ab391295d06c4d16ad8005f5
nikaxucishvil1 <xucishvilin90@gmail.com>
2024-01-29 14:45:24 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+import { clickInfo1, clickInfo2, clickInfo3, clickInfo4, clickInfo5 } from "./events/events.js";
+
+clickInfo1()
+clickInfo2()
+clickInfo3()
+clickInfo4()
+clickInfo5()
---
+++ events/events.js
@@ -0,0 +1,99 @@
+export
+
+function clickInfo1() {
+    const alwaysSpan = document.querySelectorAll("#alwaysSpan");
+    const startI = document.querySelectorAll("#startI");
+    const noneSpan = document.querySelectorAll("#noneSpan");
+    const flipI = document.querySelectorAll("#flipI");
+    const click = document.querySelectorAll("#button");
+  
+    click[0].addEventListener("focusin", () => {
+      startI[0].style.display = "none";
+      flipI[0].style.display = "block";
+      noneSpan[0].style.display = "block";
+    });
+    click[0].addEventListener("focusout", () => {
+      startI[0].style.display = "block";
+      flipI[0].style.display = "none";
+      noneSpan[0].style.display = "none";
+    });
+  }
+  export
+  function clickInfo2() {
+    const alwaysSpan = document.querySelectorAll("#alwaysSpan");
+    const startI = document.querySelectorAll("#startI");
+    const noneSpan = document.querySelectorAll("#noneSpan");
+    const flipI = document.querySelectorAll("#flipI");
+    const click = document.querySelectorAll("#button");
+    
+    click[1].addEventListener("focusin", () => {
+      startI[1].style.display = "none";
+      flipI[1].style.display = "block";
+      noneSpan[1].style.display = "block";
+    });
+    click[1].addEventListener("focusout", () => {
+      startI[1].style.display = "block";
+      flipI[1].style.display = "none";
+      noneSpan[1].style.display = "none";
+    });
+  }
+
+  export
+  function clickInfo3() {
+    const alwaysSpan = document.querySelectorAll("#alwaysSpan");
+    const startI = document.querySelectorAll("#startI");
+    const noneSpan = document.querySelectorAll("#noneSpan");
+    const flipI = document.querySelectorAll("#flipI");
+    const click = document.querySelectorAll("#button");
+  
+    click[2].addEventListener("focusin", () => {
+      startI[2].style.display = "none";
+      flipI[2].style.display = "block";
+      noneSpan[2].style.display = "block";
+    });
+    click[2].addEventListener("focusout", () => {
+      startI[2].style.display = "block";
+      flipI[2].style.display = "none";
+      noneSpan[2].style.display = "none";
+    });
+  }
+  export
+  function clickInfo4() {
+    const alwaysSpan = document.querySelectorAll("#alwaysSpan");
+    const startI = document.querySelectorAll("#startI");
+    const noneSpan = document.querySelectorAll("#noneSpan");
+    const flipI = document.querySelectorAll("#flipI");
+    const click = document.querySelectorAll("#button");
+  
+    click[3].addEventListener("focusin", () => {
+      startI[3].style.display = "none";
+      flipI[3].style.display = "block";
+      noneSpan[3].style.display = "block";
+    });
+    click[3].addEventListener("focusout", () => {
+      startI[3].style.display = "block";
+      flipI[3].style.display = "none";
+      noneSpan[3].style.display = "none";
+    });
+  }
+  export
+  function clickInfo5() {
+    const alwaysSpan = document.querySelectorAll("#alwaysSpan");
+    const startI = document.querySelectorAll("#startI");
+    const noneSpan = document.querySelectorAll("#noneSpan");
+    const flipI = document.querySelectorAll("#flipI");
+    const click = document.querySelectorAll("#button");
+  
+    click[4].addEventListener("focusin", () => {
+      startI[4].style.display = "none";
+      flipI[4].style.display = "block";
+      noneSpan[4].style.display = "block";
+    });
+    click[4].addEventListener("focusout", () => {
+      startI[4].style.display = "block";
+      flipI[4].style.display = "none";
+      noneSpan[4].style.display = "none";
+    });
+  }
+  
+  
